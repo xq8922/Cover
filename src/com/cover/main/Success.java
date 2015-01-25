7222424bb60f80c0b56e248f7f71a765108b779e
xq8922 <xq8922@foxmail.com>
2015-01-25 22:08:46 +0800

    update

--- src/com/cover/main/Success.java
+++ src/com/cover/main/Success.java
@@ -1,12 +1,12 @@
 package com.cover.main;
 
-import com.wxq.covers.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.wxq.covers.R;
 
 public class Success extends Activity {
 	Button btn_send;
